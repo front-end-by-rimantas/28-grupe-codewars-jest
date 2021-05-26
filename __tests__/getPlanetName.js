04c96ed19223c9721d6b0616fa2d6365f6a65611
Rimantas <belauzas@yahoo.com>
2021-05-26 18:30:37 +0300

    Initial commit

---
+++ __tests__/getPlanetName.js
@@ -0,0 +1,29 @@
+function getPlanetName(id) {
+    var name;
+    switch (id) {
+        case 1:
+            name = 'Mercury'
+        case 2:
+            name = 'Venus'
+        case 3:
+            name = 'Earth'
+        case 4:
+            name = 'Mars'
+        case 5:
+            name = 'Jupiter'
+        case 6:
+            name = 'Saturn'
+        case 7:
+            name = 'Uranus'
+        case 8:
+            name = 'Neptune'
+    }
+
+    return name;
+}
+
+test('teisingi planetu pavadinimai', () => {
+    expect(getPlanetName(2)).toBe('Venus');
+    expect(getPlanetName(3)).toBe('Earth');
+    expect(getPlanetName(5)).toBe('Jupiter');
+})
